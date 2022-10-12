d224da78ba7444c0ce0cae178216707db04e4bd3
Deepak <er.deepakgoyal786@gmail.com>
2022-10-13 00:03:38 +0530

    Display shotstack magic is happening content

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "0c32d3c3c34230366e20babd50e58df6",
-"index.html": "73e51d160046e7c45a1799198768563d",
-"/": "73e51d160046e7c45a1799198768563d",
-"main.dart.js": "4a31bd542ad1aa96593796d999a838c6",
+  "version.json": "942a77df5d7f8768b1f26320fd7775d1",
+"index.html": "7118b74786ddfd5549d114c2a10911da",
+"/": "7118b74786ddfd5549d114c2a10911da",
+"main.dart.js": "505c0386c8b9d2676a95ffcd05409c93",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "upload_file_function.js": "9924ee89dfc287638b587b568f25adfb",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
