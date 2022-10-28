bf338240f01687ad2c38db12a49d3d9e6f50703a
Deepak <er.deepakgoyal786@gmail.com>
2022-10-29 03:10:12 +0530

    Set the data-sl attribute to canvas element

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "a554cb2975bec1291354558e2453a6fe",
-"index.html": "e0d2e1020b70d4f9475ba9a7ea205321",
-"/": "e0d2e1020b70d4f9475ba9a7ea205321",
+"index.html": "661a9c348e1b866a1237d8e34eca9cdb",
+"/": "661a9c348e1b866a1237d8e34eca9cdb",
 "main.dart.js": "a26b0cf83371c50302bd51442a4d3c8f",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "upload_file_function.js": "9924ee89dfc287638b587b568f25adfb",
