6b5953ebf31b132cb50a43db20e47b5f97a94dd9
Deepak <er.deepakgoyal786@gmail.com>
2022-11-07 21:47:29 +0530

    Use maxWidth based on screenHeight

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3933745128b73c458d7dea713547655d",
-"index.html": "724736d15595f9284b2aa1b59c4a96d5",
-"/": "724736d15595f9284b2aa1b59c4a96d5",
-"main.dart.js": "e9d661b297381e47a5be2ab3e5054d6a",
+"index.html": "6789512aa8fd5498c54c752935365aef",
+"/": "6789512aa8fd5498c54c752935365aef",
+"main.dart.js": "16eb1f2192bcd8aed30a6650fc7cac72",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "upload_file_function.js": "9924ee89dfc287638b587b568f25adfb",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
