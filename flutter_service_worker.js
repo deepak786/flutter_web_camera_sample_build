e305035f35eb16e0bd01c7ca6308b1ac30822420
Deepak <er.deepakgoyal786@gmail.com>
2024-12-10 22:18:41 +0530

    Test Enhanced conversions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "3aa69e2364a01476f92de3136b3afc10",
-"index.html": "489e120ccdc908df275f2e374a2b8eb1",
-"/": "489e120ccdc908df275f2e374a2b8eb1",
+"index.html": "6ed5a9cf12ff857b169e3dbb78ef3918",
+"/": "6ed5a9cf12ff857b169e3dbb78ef3918",
 "firebase-messaging-sw.js.map": "48fe9d86dd5c86bfb4b7b56c84eb2be0",
 "firebase-messaging-sw.js": "69221a3921da0521c1e758b4779e1f73",
-"main.dart.js": "48b807063e840af2af73c8911178f310",
+"main.dart.js": "05eedf7915a2dd3b4134adb85988a837",
 "worker.js": "f916974921b8b3d98f9fdfc4b68e5432",
 "upload_file_function.js": "9924ee89dfc287638b587b568f25adfb",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
